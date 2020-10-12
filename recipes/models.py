29d7c02a2241c23a060c0a82f4a1f98144526aef
Черезов Артем <avcherezov@gmail.com>
2020-10-12 22:56:54 +0300

    added apps + postgresql

---
+++ recipes/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model() 
+
+
+#class Tags(models.Model):
+#    pass
+
+
+#class Ingredients(models.Model):
+#    pass
+
+
+#class Recipe(models.Model):
+#    pass
+
